455c2416bbe4611d1b08f822bea1f6c4940d97f4
maxami123 <synard.maxwell@gmail.com>
2021-11-30 21:40:38 -0800

    Create RTC.ino

---
+++ TestCode/RTC/RTC.ino
@@ -0,0 +1,30 @@
+#include "RTClib.h"
+
+RTC_DS1307 RTC;
+
+void setup () {
+  Serial.begin(9600);
+  RTC.begin();
+
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+}
+
+void loop () {
+  DateTime now = RTC.now();
+  Serial.print(now.year());
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC); 
+  Serial.println();
+  delay(1000);
+}
